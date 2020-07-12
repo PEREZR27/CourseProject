f8b5cf0cd6e39bd2204c23b083119b00bcea2819
Richard Perez <PEREZR27@my.erau.edu>
2020-07-12 15:42:53 -0500

    Finished Product

--- ProgEX1_/ProgEX1_/Program.cs
+++ ProgEX1_/ProgEX1_/Program.cs
@@ -4,6 +4,9 @@ namespace progex01
 {
     class Program
     {
+        private static int bottom;
+        private static int height;
+
         // first comment
         static void Main(string[] args)
         {
@@ -17,15 +20,18 @@ namespace progex01
             Console.WriteLine($"The circumference is {circumference}");
 
             // Implementation for area here
-            double area = 0;
+            double area = Math.PI * Math.Pow(intradius, 2);
             Console.WriteLine($"The area is {area}");
 
 
             // Part 2
             Console.WriteLine("\nPart 2, volume of a hemisphere.");
-
             // Implementation here
-            double volume = 0;
+            Console.Write("Enter an integer for the radius: ");
+            string strradius1 = Console.ReadLine();
+            int intradius1 = int.Parse(strradius1);
+            double v2 = 2 / 3.0 * Math.PI * Math.Pow(intradius1, 3);
+            double volume = v2;
             Console.WriteLine($"The volume is {volume}");
 
 
@@ -33,19 +39,41 @@ namespace progex01
             Console.WriteLine("\nPart 3, area of a triangle (Heron's formula).");
 
             // Implementation here
-
-            Console.WriteLine($"The area is {area}");
+            Console.Write("Enter an integer for the bottom ");
+            string strbottom = Console.ReadLine();
+            int intbottom = int.Parse(strbottom);
+            Console.Write("Enter an integer for the height ");
+            string strheight = Console.ReadLine();
+            int intheight = int.Parse(strheight);
+            double v1 = intbottom * intheight * .5;
+            double area1 = v1;
+            Console.WriteLine($"The area is {area1}");
 
 
             // Part 4
             Console.WriteLine("\nPart 4, solving a quadratic equation.");
 
             // Implementation here
-            double positive_num = 1;
-            double negative_num = -1;
-            double denominator = 2;
-            Console.WriteLine($"The positive solution is {positive_num / denominator}");
-            Console.WriteLine($"The negative solution is {negative_num / denominator}");
+            Console.Write("Enter an integer for the a ");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write("Enter an integer for the b ");
+            float b = float.Parse(Console.ReadLine());
+            Console.Write("Enter an integer for the c ");
+            float c = float.Parse(Console.ReadLine()) ;
+            double deltaRoot = Math.Sqrt(b * b - 4 * a * c);
+
+                if(deltaRoot >= 0){ 
+            double x1 = ( -b + deltaRoot) / 2.0 * a;
+            double x2 = ( b + deltaRoot) / 2.0 * a;
+            Console.WriteLine($"The positive solution is { x1 }");
+            Console.WriteLine($"The negative solution is  { x2 }");
+            }
+            else
+            {
+                Console.WriteLine($"there are no roots");
+            }
+            
+            
         }
     }
 }
