c41337e53717859df137e3845f9a2d39cd079605
Richard Perez <PEREZR27@my.erau.edu>
2020-07-27 15:00:56 -0500

    Added Requirements

--- ProgEX1_/ProgEX1_/Program.cs
+++ ProgEX1_/ProgEX1_/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Linq.Expressions;
 
-namespace progex01
+namespace progex03c
 {
     class Program
     {
@@ -12,72 +13,167 @@ namespace progex01
         {
             // Part 1
             // Partially worked example
-            Console.WriteLine("\nPart 1, circumference and area of a circle.");
-            Console.Write("Enter an integer for the radius: ");
-            string strradius = Console.ReadLine();
-            int intradius = int.Parse(strradius);
-            double circumference = 2 * Math.PI * intradius;
-            Console.WriteLine($"The circumference is {circumference}");
+            try
+            {
+                Console.WriteLine("\nPart 1, circumference of a circle.");
+                Console.Write("Enter an integer for the radius: ");
+                string strradius = Console.ReadLine();
+                int intradius = int.Parse(strradius);
+                while (intradius != 0)
+                {
+                    checked
+                    {
+                        double circumference = 2 * Math.PI * intradius;
+                        Console.WriteLine($"The circumference is {circumference}");
+                    }
+                    break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Not a valid integer. Try again.");
+            }
+            finally
+            {
+                Console.WriteLine("This program has finally terminated.");
+            }
+            try
+            {
+                Console.WriteLine("\nPart 1-2, area of a circle.");
+                Console.Write("Enter an integer for the radius: ");
+                string strradius = Console.ReadLine();
+                int intradius = int.Parse(strradius);
+                while (intradius != 0)
+                {
+                    checked
+                    {
+                        double area = Math.PI * Math.Pow(intradius, 2);
+                        Console.WriteLine($"The area is {area}");
+                        break;
+                    }
 
-            // Implementation for area here
-            double area = Math.PI * Math.Pow(intradius, 2);
-            Console.WriteLine($"The area is {area}");
+                }
 
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Not a valid integer. Try again.");
+            }
+            finally
+            {
+                Console.WriteLine("This program has finally terminated.");
+            }
+            try
+            {
+                // Part 2
+                Console.WriteLine("\nPart 2, volume of a hemisphere.");
+                // Implementation here
+                Console.Write("Enter an integer for the radius: ");
+                string strradius1 = Console.ReadLine();
+                int intradius1 = int.Parse(strradius1);
+                while (intradius1 != 0)
+                {
+                    checked
+                    {
+                        double v2 = 2 / 3.0 * Math.PI * Math.Pow(intradius1, 3);
+                        double volume = v2;
+                        Console.WriteLine($"The volume is {volume}");
+                        break;
+                    }
 
-            // Part 2
-            Console.WriteLine("\nPart 2, volume of a hemisphere.");
-            // Implementation here
-            Console.Write("Enter an integer for the radius: ");
-            string strradius1 = Console.ReadLine();
-            int intradius1 = int.Parse(strradius1);
-            double v2 = 2 / 3.0 * Math.PI * Math.Pow(intradius1, 3);
-            double volume = v2;
-            Console.WriteLine($"The volume is {volume}");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Not a valid integer. Try again.");
+            }
+            finally
+            {
+                Console.WriteLine("This program has finally terminated.");
+            }
+            try
+            {
+                // Part 3
+                Console.WriteLine("\nPart 3, area of a triangle (Heron's formula).");
 
+                // Implementation here
 
-            // Part 3
-            Console.WriteLine("\nPart 3, area of a triangle (Heron's formula).");
+                Console.Write("Enter an integer for the bottom ");
+                string strbottom = Console.ReadLine();
+                int intbottom = int.Parse(strbottom);
+                Console.Write("Enter an integer for the height ");
+                string strheight = Console.ReadLine();
+                int intheight = int.Parse(strheight);
+                while (intheight != 0 && intbottom != 0)
+                {
+                    checked
+                    {
+                        double v1 = intbottom * intheight * .5;
+                        double area1 = v1;
+                        Console.WriteLine($"The area is {area1}");
+                        break;
+                    }
 
-            // Implementation here
-            Console.Write("Enter an integer for the bottom ");
-            string strbottom = Console.ReadLine();
-            int intbottom = int.Parse(strbottom);
-            Console.Write("Enter an integer for the height ");
-            string strheight = Console.ReadLine();
-            int intheight = int.Parse(strheight);
-            double v1 = intbottom * intheight * .5;
-            double area1 = v1;
-            Console.WriteLine($"The area is {area1}");
-
-
-            // Part 4
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Not a valid integer. Try again.");
+            }
+            finally
+            {
+                Console.WriteLine("This program has finally terminated.");
+            }
             Console.WriteLine("\nPart 4, solving a quadratic equation.");
 
-            // Implementation here
-            Console.Write("Enter an integer for the a ");
-            float a = float.Parse(Console.ReadLine());
-            Console.Write("Enter an integer for the b ");
-            float b = float.Parse(Console.ReadLine());
-            Console.Write("Enter an integer for the c ");
-            float c = float.Parse(Console.ReadLine()) ;
-            double deltaRoot = Math.Sqrt(b * b - 4 * a * c);
 
-                if(deltaRoot >= 0){ 
-            double x1 = ( -b + deltaRoot) / 2.0 * a;
-            double x2 = ( b + deltaRoot) / 2.0 * a;
-            Console.WriteLine($"The positive solution is { x1 }");
-            Console.WriteLine($"The negative solution is  { x2 }");
-            }
-            else
-            {
-                Console.WriteLine($"there are no roots");
-            }
-            
+
+            // Implementation here
+
+            Console.Write("Enter an integer for the a ");
+
+            float a = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter an integer for the b ");
+
+            float b = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter an integer for the c ");
+
+            float c = float.Parse(Console.ReadLine());
+
+                while ((b * b - 4 * a * c) > 0)
+                {
+                checked
+                {
+                    double deltaRoot = Math.Sqrt(b * b - 4 * a * c);
+
+                    if (deltaRoot >= 0)
+                    {
+
+                        double x1 = (b + deltaRoot) / 2.0 * a;
+
+                        double x2 = (-b + deltaRoot) / 2.0 * a;
+
+                        Console.WriteLine($"The positive solution is { x1 }");
+
+                        Console.WriteLine($"The negative solution is  { x2 }");
+
+                    }
+
+                    else
+
+                    {
+
+                        Console.WriteLine($"there are no roots");
+
+                    }
+                    break;
+                }
+                }
             
         }
     }
 }
-
-
 
 
---
+++ docs/user-need-statement/Requirements.md
@@ -0,0 +1,120 @@
+#Requirements
+- As a User I can create an account:
+1. User clicks on account creation option
+2. User is prompted to fill in the following fields:
+First Name
+Last Name
+Username
+Password
+Password hint
+3. The following rules will apply to each field
+First and Last Name: text only fields. Limited to 60 characters
+Username: text and numeric field. Limited to 20 characters
+Password: must be at least 9 characters, with at least 1 uppercase letter, 1 number and 1 special character
+
+-As a User I can login to my account:
+1. User clicks on account login option
+2. User is prompted to fill in the following fields:
+Username
+Password
+- If the user unsuccesfully logs in after three attempts the will be prompted to try again or ask for hint.
+-The hint will be displayed
+
+-As a user I can reset my password:
+1. User clicks on account reset password option
+2. User is prompted to fill in the following fields:
+Previous password
+New password x2
+3. The following rules will apply to each field
+Password: must be at least 9 characters, with at least 1 uppercase letter, 1 number and 1 special character
+
+-As a a User once I login for the first time I should see a Welcome banner
+1. The user completes their account set up and logs in successfully for the first time.
+-A banner will read "Welcome, to your 8 week fitness guide"
+
+-As a a User once I login for the first time I should see a Purpose statement after the welcome banner
+1. The user completes their account set up and logs in successfully for the first time.
+2. The user will see the welcome banner and click next in the bottom right corner
+-The user will see a Purpose statement which will read a mission statement like
+"This fitness program focuses on improving physical performance through excercise and nutrition. It provides photos and descriptions of excercises."
+
+-As a a User once I login for the first time I should see a How to use guide after the purpose statement
+1. The user completes their account set up and logs in successfully for the first time.
+2. The user will see the welcome banner and click next in the bottom right corner
+3.The user will see the purpose statement and click next in the bottom right corner
+-The user will see a user guide which will tell the user how to use the app
+"This app uses ab 8 week fitness routine. It recommended you start on a monday and progress through the program everyday.If you miss a day, thats fine; just shift your schedule, and continue the program in its intended order."
+
+
+-As a user I should be able to view my excercise guide weeks 1-8 
+1.The user will login and see there 8 week program laid out in front of them
+-Users can choose to look at any week
+
+-As a User I should be able to choose a week and be able to choose any day within that week
+1.The user can select any week 1-8 and see a list of all the days they will train that week labeled 1-5.
+-Since it is a 5 day 8 week program there is no point to label them by day.
+
+-As a user I should be able to choose a day and see my routine for that day
+1. The user will choose a day that corresponds with a given week and inside that day they will see there workout routine.
+
+-As a user I should be able to see a photograph along with a detailed description of each workout inside of each day
+1.The user will choose a workout inside of a daily routine and see the results
+- A photograph depicting the excercise and a step by step instruction guide on how perform the excercise
+
+-As a user I should be able to mark workouts I have already completed
+1. The user will check a box besides a week or day
+-This will help the user remember where they left off.
+
+-As a user I should be able to mark excercises I have already completed
+1. The user will check a box besides a excercise
+-This will help the user remember where they left off.
+
+-As a User I once I click the menu button I should see a menu
+1.The user will click the menu icon
+2. A dropdown will appear with a list of choices for the user to select
+-Help
+-Nutrition
+-Rest / Recovery
+-Excercise List
+-Required Equipment
+-Reset Data
+
+-As a User I should be able to access the Welcome Banner, Purpose Statement, and How to use guide at any point by clicking a icon on the drop down
+1.User will select drop down in top left corner
+2.User will select Help
+-The Welcome banner, Purpose Statement, and User guide will all be available choices under help
+
+-As a User I should be able to access the nutrition page at any point by clicking a icon on the drop down
+1.User will select drop down in top left corner
+2.User will select Nutrition
+-The nutrition page will suggest caloric intake, proteins, fats, and carbohydrates.
+-Will feature a calorie calculator based on body dimensions such as height and weight
+
+-As a User I should be able to access the Rest / Recovery guide at any point by clicking a icon on the drop down
+1.User will select drop down in top left corner
+2.User will select Rest / Recovery
+-This is a page full of strecthes and suggestions for the user to follow on rest days
+
+-As a User I should be able to access the Excercise List at any point by clicking a icon on the drop down
+1.User will select drop down in top left corner
+2.User will select Excercise List
+-This page will containt every excercise used in the eight week program
+
+-As a User I should be able to access the Required Equipment at any point by clicking a icon on the drop down
+1.User will select drop down in top left corner
+2.User will select Required Equipment
+-This page will list all the equipment a user will need for the 8 week program
+
+-As a User I should be able to access the Reset Data at any point by clicking a icon on the drop down
+1.User will select drop down in top left corner
+2.User will select Reset Data
+3.A screen will pop up and ask the user if they are sure they want to reset their data and two buttons will appear that say yes or no
+-This will uncheck all boxes on the weeks, days, and workouts.
+
+
+
+
+
+
+
+
