9ef98bb6a128efe270ebf6b5fe034df5d5d3fecc
Richard Perez <PEREZR27@my.erau.edu>
2020-10-28 13:33:03 -0500

    Updates

---
+++ Static/MVC/MVCTemp/MVCTemp/Views/Shared/bodyMass.cs
@@ -0,0 +1,62 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace MVCTemp
+//{
+//    public class bodyMass
+//    {
+//        private static void Main(string[] args)
+//        {
+//         
+//            double Scale = BMI(height, weight);
+
+//            Console.ReadKey();
+//        }
+//        static int FindMin(int a, int b)
+//        {
+//            int lowestOne = 0;
+
+//            if (a < b)
+//            {
+//                lowestOne = a;
+//            }
+//            if (a > b)
+//            {
+//                lowestOne = b;
+//            }
+
+//            return lowestOne;
+//        }
+//        static double BMI(double height, double weight)
+//        {
+//            double bMI = (weight * 703) / (height * height);
+
+//            if (bMI < 16)
+//            {
+//                Console.WriteLine($"Your BMI is {bMI}. You must add weight");
+//            }
+//            if (16 <= bMI && bMI < 18.5)
+//            {
+//                Console.WriteLine($"Your BMI is {bMI}. You should add weight");
+//            }
+//            if (18.5 <= bMI && bMI < 25)
+//            {
+//                Console.WriteLine($"Your BMI is {bMI}. Maintain your weight");
+//            }
+//            if (25 <= bMI && bMI < 30)
+//            {
+//                Console.WriteLine($"Your BMI is {bMI}. You should lose some weight.");
+//            }
+//            if (30 <= bMI)
+//            {
+//                Console.WriteLine($"Your BMI is {bMI}. You should lose some weight.");
+//            }
+//            return bMI;
+//        }
+//    }
+
+
+//}
